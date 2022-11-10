78e8970453a09d875dda4b141fa8289d141506bf
Melwin Kramer <melwin.kramer@gmail.com>
2022-11-10 11:42:07 +0100

    test: added tests for empty pdfs

---
+++ src/__tests__/api/emptyPdf.test.ts
@@ -0,0 +1,33 @@
+import {PlatformTest} from "@tsed/common";
+import SuperTest from "supertest";
+import {Server} from "../../Server";
+
+describe("API", () => {
+    let request: SuperTest.SuperTest<SuperTest.Test>;
+
+    beforeEach(PlatformTest.bootstrap(Server));
+    beforeEach(() => {
+        request = SuperTest(PlatformTest.callback());
+    });
+
+    afterEach(PlatformTest.reset);
+
+
+    describe("POST /rest/pdf", ()=>{
+       it("creates an empty pdf", async () => {
+           const response = await request.post("/rest/pdf")
+               .send({})
+               .expect(200)
+               .expect('Content-Type', /pdf/);
+       })
+    });
+
+    describe("POST /rest/pdf/merge", ()=>{
+        it("creates a merged pdf ", async () => {
+            const response = await request.post("/rest/pdf/merge")
+                .send({})
+                .expect(200)
+                .expect('Content-Type', /pdf/);
+        })
+    });
+})
